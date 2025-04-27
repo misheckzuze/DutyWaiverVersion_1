6d22949ae776e2255e59ef1703ceb560f7103e62
cwisiki <cwisiki@mra.mw>
2025-04-27 13:43:47 +0200

    WIP

--- src/components/duty-waiver/application-form/edit-applications/EditApplication.tsx
+++ src/components/duty-waiver/application-form/edit-applications/EditApplication.tsx
@@ -107,8 +107,8 @@ export default function EditApplicationForm({ id }: EditApplicationFormProps) {
         startDate: projectDetails.startDate?.toISOString().split('T')[0] || "",
         endDate: projectDetails.endDate?.toISOString().split('T')[0] || "",
         attachments: attachments.map(att => ({
-          documentType: att.type,
-          filePath: att.file?.name || ""
+          documentType: att.documentType,
+          filePath: att.filePath || ""
         })),
         items: items.map(item => ({
           description: item.description,
--- src/types/AttachmentModel.ts
+++ src/types/AttachmentModel.ts
@@ -1,5 +1,7 @@
 export interface Attachment {
     id: string;
     type: string;
+    documentType: string;
+    filePath: string;
     file: File | null;
   }
