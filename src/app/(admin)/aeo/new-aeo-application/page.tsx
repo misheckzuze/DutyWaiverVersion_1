87fad351026a13e29577f6ad3679d43de55b77c4
cwisiki <cwisiki@mra.mw>
2025-09-04 12:13:11 +0200

    Implementing AEO Portal

--- src/app/(admin)/aeo/new-aeo-application/page.tsx
+++ src/app/(admin)/aeo/new-aeo-application/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import PageBreadCrumb from "@/components/common/PageBreadCrumb";
+import AEOForm from "@/components/aeo/AEOForm";
 
 export default function NewAeoApplication() {
   return (
     <div>
       <PageBreadCrumb pageTitle="New AEO Application" />
-      <div className="p-6 bg-white dark:bg-gray-800 rounded-lg shadow">
-        <h3 className="text-lg font-medium text-gray-800 dark:text-white">New AEO Application</h3>
-        <p className="text-sm text-gray-500 mt-2">Sample form page for creating a new AEO application.</p>
+      <div className="p-6">
+        <AEOForm />
       </div>
     </div>
   );
